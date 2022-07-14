a1979ad639b013683b63d6cad7b842f25603875d
Zahar A. Ponimash <ponimashz@mail.ru>
2022-07-15 01:59:28 +0300

    AGC

---
+++ Tests/DSPTests/AGCTest/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace AGCTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chartVisual1 = new AI.Charts.Control.ChartVisual();
+            this.chartVisual2 = new AI.Charts.Control.ChartVisual();
+            this.SuspendLayout();
+            // 
+            // chartVisual1
+            // 
+            this.chartVisual1.AutoScroll = true;
+            this.chartVisual1.BackColor = System.Drawing.Color.White;
+            this.chartVisual1.ChartName = "График";
+            this.chartVisual1.ForeColor = System.Drawing.Color.Black;
+            this.chartVisual1.IsContextMenu = true;
+            this.chartVisual1.IsLogScale = false;
+            this.chartVisual1.IsMoove = true;
+            this.chartVisual1.IsScale = true;
+            this.chartVisual1.IsShowXY = true;
+            this.chartVisual1.LabelX = "Ось Х";
+            this.chartVisual1.LabelY = "Ось Y";
+            this.chartVisual1.Location = new System.Drawing.Point(3, 13);
+            this.chartVisual1.Name = "chartVisual1";
+            this.chartVisual1.Size = new System.Drawing.Size(929, 302);
+            this.chartVisual1.TabIndex = 0;
+            // 
+            // chartVisual2
+            // 
+            this.chartVisual2.AutoScroll = true;
+            this.chartVisual2.BackColor = System.Drawing.Color.White;
+            this.chartVisual2.ChartName = "График";
+            this.chartVisual2.ForeColor = System.Drawing.Color.Black;
+            this.chartVisual2.IsContextMenu = true;
+            this.chartVisual2.IsLogScale = false;
+            this.chartVisual2.IsMoove = true;
+            this.chartVisual2.IsScale = true;
+            this.chartVisual2.IsShowXY = true;
+            this.chartVisual2.LabelX = "Ось Х";
+            this.chartVisual2.LabelY = "Ось Y";
+            this.chartVisual2.Location = new System.Drawing.Point(3, 322);
+            this.chartVisual2.Name = "chartVisual2";
+            this.chartVisual2.Size = new System.Drawing.Size(929, 302);
+            this.chartVisual2.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(944, 672);
+            this.Controls.Add(this.chartVisual2);
+            this.Controls.Add(this.chartVisual1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AI.Charts.Control.ChartVisual chartVisual1;
+        private AI.Charts.Control.ChartVisual chartVisual2;
+    }
+}
+
---
+++ Tests/DSPTests/AGCTest/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AI.DataStructs.Algebraic;
+using AI.SignalLab.Generators;
+using AIDog.DataPrep.Base.Seq1D;
+
+namespace AGCTest
+{
+    public partial class Form1 : Form
+    {
+
+
+        RectGenerator generator = new RectGenerator();
+        Vector data = new Vector(0), agc_dat = new Vector(0);
+        AGC aGC = new AGC();
+
+        public Form1()
+        {
+            InitializeComponent();
+            aGC.Treshold = 4;
+            aGC.Treshold2 = 4;
+
+            generator.PeriodMS = 120;
+            generator.freq = 70;
+            generator.A = 10000;
+            generator.Start();
+            generator.SignalSemple += Generator_SignalSemple;
+        }
+
+        private void Generator_SignalSemple(double obj)
+        {
+            data.Add(obj);
+            agc_dat.Add(aGC.Calculate(obj));
+            chartVisual1.PlotBlack(data);
+            chartVisual2.PlotBlack(agc_dat);
+        }
+
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            generator.Dispose();
+        }
+    }
+}
